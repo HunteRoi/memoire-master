41c94d75e4ee4acbe6f8099baffe42ca2d378ccc
HunteRoi <me@tinaeldevresse.eu>
2025-08-29 16:36:25 +0200

    feat: add publisher for github

--- src/client/forge.config.ts
+++ src/client/forge.config.ts
@@ -7,6 +7,7 @@ import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-nati
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import { PublisherGithub } from '@electron-forge/publisher-github';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -22,6 +23,14 @@ const config: ForgeConfig = {
     new MakerZIP({}, ['darwin']),
     new MakerRpm({}),
     new MakerDeb({}),
+  ],
+  publishers: [
+    new PublisherGithub({
+      repository: {
+        owner: 'hunteroi',
+        name: 'memoire-master'
+      }
+    })
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
